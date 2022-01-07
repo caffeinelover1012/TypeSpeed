f9ecc45b89a5c3f7ace956bb895192a011a53d0c
Shubh Khandelwal <shubhk5020@gmail.com>
2022-01-07 04:20:57 -0700

    changed imgur in footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <div className="footer">
       <hr style={{"width":'30%','textDecorationStyle:':'solid',"minWidth":'400px'}}/>
-    <h2 style={{'margin':'2px 0 2px 0'}}>&copy; 2022 &nbsp; | &nbsp; created by Shubh Khandelwal &nbsp;|<a href="https://www.instagram.com/caffeinetocode"> <img alt="insta" src={'https://imgur.com/a/Rn2AGa9'} style={{'margin':'-10px 0 -6px 0', 'vertical-align': 'bottom'}}/></a></h2>
+    <h2 style={{'margin':'2px 0 2px 0'}}>&copy; 2022 &nbsp; | &nbsp; created by Shubh Khandelwal &nbsp;|<a href="https://www.instagram.com/caffeinetocode"> <img alt="insta" src={'https://i.imgur.com/1CNFwqf.png'} style={{'margin':'-10px 0 -6px 0', 'vertical-align': 'bottom'}}/></a></h2>
   </div>
 );
 
